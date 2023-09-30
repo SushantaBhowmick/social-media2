88a610138740d3f8985410f8bed67b632448b74e
Sushanta Bhowmick <bhosushanta922@gmail.com>
2023-09-30 23:05:38 +0530

    add post and right bar with some dummy data

--- frontnd/src/components/feed/Feed.jsx
+++ frontnd/src/components/feed/Feed.jsx
@@ -2,13 +2,23 @@ import React from 'react'
 import "./Feed.css"
 import Share from '../share/Share'
 import Post from '../post/Post'
+import {Posts} from '../../dummyData'
 
 const Feed = () => {
   return (
     <div className='feed'>
       <div className="feeedWrapper">
         <Share />
-        <Post />
+          {
+            Posts.map((p)=>(
+              <Post
+              key={p.id}
+              post={p}
+              />
+
+            ))
+          }
+       
       </div>
     </div>
   )
--- frontnd/src/components/post/Post.jsx
+++ frontnd/src/components/post/Post.jsx
@@ -1,21 +1,37 @@
 import React from 'react'
 import "./post.css"
-const Post = () => {
+import MoreVert from '@mui/icons-material/MoreVert'
+
+const Post = (post) => {
+    console.log(post)
+    console.log(post.datew)
   return (
     <div className='post'>
         <div className="postWrapper">
             <div className="postTop">
                 <div className="postTopLeft">
-                    <img src="/assest/person/1.jpeg" alt="" className="postProfileImg" />
+                    <img src="/assets/person/1.jpeg" alt="" className="postProfileImg" />
                     <span className="postUserName">Sushanta Bhowmick</span>
-                    <span className="postDate">5 mins ago</span>
+                    <span className="postDate">{post.date}</span>
                 </div>
                 <div className="postTopRight">
-                    
+                    <MoreVert />
                 </div>
             </div>
-            <div className="postCenter"></div>
-            <div className="postRight"></div>
+            <div className="postCenter">
+                <div className="postText">{post.desc}</div>
+                <img src="/assets/post/1.jpeg" alt="" className="postImg" />
+            </div>
+            <div className="postBottom">
+                <div className="postBottomLeft">
+                    <img className='likeIcon' src="/assets/like.png" alt="" />
+                    <img className='likeIcon' src="/assets/heart.png" alt="" />
+                    <span className="postLikeCounter">{post.like} People Liked</span>
+                </div>
+                <div className="postBottomRight">
+                    <div className="postCommentText">{post.comment} comments</div>
+                </div>
+            </div>
         </div>
     </div>
   )
